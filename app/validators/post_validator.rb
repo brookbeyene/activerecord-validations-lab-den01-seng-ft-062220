b0edf05bf28c12459d283d7a922af478f68aee0b
Brook Beyene <brookbeyene37@gmail.com>
2020-08-03 23:06:31 +0000

    Automatically backed up by Learn

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -2,4 +2,7 @@ class PostValidator < ActiveModel::Validator
   def Validate(record)
     unless record.title.in("Won't Believe", "Secret", "Top [number]") 
       record.error[:title] << "Validation"
+    end
+  end
+    
 end
